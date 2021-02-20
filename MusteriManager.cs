2d62cc05fd19558904d2476e83498bc6f55d3d94
Cevat Sarıkaya <cvtsrky38@gmail.com>
2021-02-20 17:22:16 +0300

    Add project files.

---
+++ MusteriManager.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        
+
+
+
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri adı : ");
+            musteri.musteriAdi = Console.ReadLine();
+            Console.WriteLine("Müşteri soyadı : ");
+            musteri.musteriSoyadi = Console.ReadLine();
+            Console.WriteLine("Müşteri id : ");
+            musteri.musteriId = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Müşteri üyelik tipi : ");
+            musteri.musteriUyelikTipi = Console.ReadLine();
+            Console.WriteLine("Müşteri başarıyla eklendi!");
+            Console.WriteLine("---------------------------------");
+            Console.WriteLine("Bütün müşteriler listeleniyor......");
+
+
+        }
+        public void Sil(Musteri musteri)
+        {
+            musteri.musteriAdi = null;
+            musteri.musteriSoyadi = null;
+            musteri.musteriId = 0;
+            musteri.musteriUyelikTipi = null;
+            Console.WriteLine("Müşteri başarıyla silindi!");
+            Console.WriteLine("---------------------------------");
+            Console.WriteLine("Kalan müşteriler listeleniyor......");
+
+        }
+        public void Listele(Musteri[] musteri)
+        {
+            foreach (Musteri musteriler in musteri)
+            {
+                if (musteriler != null & musteriler.musteriId != 0)
+                {
+                    Console.WriteLine("Müşteri adı : " + musteriler.musteriAdi);
+                    Console.WriteLine("Müşteri soyadı : " + musteriler.musteriSoyadi);
+                    Console.WriteLine("Müşteri üyelik tipi : " + musteriler.musteriUyelikTipi);
+                }
+            }
+            Console.WriteLine("Müşteriler başarıyla listelendi!");
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MusteriManager musteriManager = new MusteriManager();
+            Musteri musteri1 = new Musteri();
+            musteri1.musteriId = 1;
+            musteri1.musteriAdi = "Yusuf";
+            musteri1.musteriSoyadi = "Kaymaz";
+            musteri1.musteriUyelikTipi = "Bireysel";
+            Musteri musteri3 = new Musteri();
+            musteri3.musteriId = 3;
+            musteri3.musteriAdi = "Yusuf";
+            musteri3.musteriSoyadi = "Sarıkaya";
+            musteri3.musteriUyelikTipi = "Kurumsal";
+            Musteri musteri2 = new Musteri();
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2, musteri3 };
+
+            int secim = Giris();
+
+            switch (secim)
+            {
+                case 1:
+                    musteriManager.Ekle(musteri2);
+                    musteriManager.Listele(musteriler);
+                    break;
+                case 2:
+                    musteriManager.Sil(musteri1);
+                    musteriManager.Listele(musteriler);
+                    break;
+                case 3:
+                    musteriManager.Listele(musteriler);                   
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        static int Giris()
+        {
+            
+            Console.WriteLine("İşlem için lütfen seçim yapınız : ");
+            Console.WriteLine("Müşteri eklemek için 1 i tuşlayın! ");
+            Console.WriteLine("Müşteri silmek için 2 yi tuşlayın! ");
+            Console.WriteLine("Müşterileri listelemek için 3 ü tuşlayın! ");
+            int secim;
+            secim = Convert.ToInt32(Console.ReadLine());
+            return secim;
+        }
+    }
+}
